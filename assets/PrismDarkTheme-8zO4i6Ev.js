7f41a15a15f5153b7db5d69a1b704e096b155a50
nudopnu <forsuse@live.de>
2024-04-22 13:15:21 +0000

    Update from https://github.com/nudopnu/website/commit/2ee47c204637f3233d35ab6a5165ed00ff6d2ade

---
+++ assets/PrismDarkTheme-8zO4i6Ev.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-u-SDjGsY.js";import{g as r,s as a}from"./style-sheet-cache-dtK_AQPm.js";const o=()=>{const e=r("PrismDarkTheme"),t=e||document.styleSheets[document.styleSheets.length-1];return a("PrismDarkTheme",t),s.useEffect(()=>(e&&(e.disabled=!1),()=>{e&&(e.disabled=!0)}),[]),null};export{o as default};
